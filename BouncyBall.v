b2989e7d6123371b5ae1fad2c6c7e7dbf93de79d
DC123456789 <darrenchan611991@gmail.com>
2017-07-30 23:20:04 -0400

    - Fixed go key

--- BouncyBall.v
+++ BouncyBall.v
@@ -48,28 +48,28 @@ module BouncyBall
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
 	// image file (.MIF) for the controller.
-	vga_adapter VGA(
-		.resetn(resetn),
-		.clock(CLOCK_50),
-		.colour(colour),
-		.x(x),
-		.y(y),
-		.plot(writeEn),
-		/* Signals for the DAC to drive the monitor. */
-		.VGA_R(VGA_R),
-		.VGA_G(VGA_G),
-		.VGA_B(VGA_B),
-		.VGA_HS(VGA_HS),
-		.VGA_VS(VGA_VS),
-		.VGA_BLANK(VGA_BLANK_N),
-		.VGA_SYNC(VGA_SYNC_N),
-		.VGA_CLK(VGA_CLK)
-	);
+	// vga_adapter VGA(
+		// .resetn(resetn),
+		// .clock(CLOCK_50),
+		// .colour(colour),
+		// .x(x),
+		// .y(y),
+		// .plot(writeEn),
+		// /* Signals for the DAC to drive the monitor. */
+		// .VGA_R(VGA_R),
+		// .VGA_G(VGA_G),
+		// .VGA_B(VGA_B),
+		// .VGA_HS(VGA_HS),
+		// .VGA_VS(VGA_VS),
+		// .VGA_BLANK(VGA_BLANK_N),
+		// .VGA_SYNC(VGA_SYNC_N),
+		// .VGA_CLK(VGA_CLK)
+	// );
 	
-	defparam VGA.RESOLUTION = "160x120";
-	defparam VGA.MONOCHROME = "FALSE";
-	defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-	defparam VGA.BACKGROUND_IMAGE = "black.mif";
+	// defparam VGA.RESOLUTION = "160x120";
+	// defparam VGA.MONOCHROME = "FALSE";
+	// defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+	// defparam VGA.BACKGROUND_IMAGE = "black.mif";
 			
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
@@ -87,7 +87,7 @@ module BouncyBall
 	// Controller keys
 	assign left_key = !KEY[3];
 	assign right_key = !KEY[0];
-	assign go_key = KEY[1];
+	assign go_key = !KEY[1];
 
     // Instantiate datapath
     datapath D0(
--- testingBouncyBall.do
+++ testingBouncyBall.do
@@ -10,5 +10,11 @@ force {KEY} 2#0000
 force {CLOCK_50} 0 0ns, 1 5 ns -r 10 ns
 run 10 ns
 force {SW} 2#00000001
+force -deposit {C0/current_state} 10#13
+force -deposit {D0/ball_x} 10#1
+force -deposit {D0/ball_y} 10#113
+force -deposit {D0/paddle_x} 10#1
+force -deposit {D0/ball_direction} 2#1
+force -deposit {C0/current_state} 10#13
 force {CLOCK_50} 0 0ns, 1 5 ns -r 10 ns
-run 700 ns
+run 30 ns
